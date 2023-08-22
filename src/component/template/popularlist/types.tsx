eefb481d51ca472f45a96b674c1b8debc29d931e
Lyndell <lyndell.dobluis@gmail.com>
2023-08-22 11:56:51 +0800

    add unit test coverage for popular list component

---
+++ src/component/template/popularlist/types.tsx
@@ -0,0 +1,11 @@
+export type PopularListProps = {
+  data: PopularListDetails[];
+};
+
+export type PopularListDetails = {
+  image: string;
+  city: string;
+  state: string;
+  matches: string;
+  salary: string;
+};
