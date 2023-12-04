2e57228fc4405d1494d135fe04a3d4056a60f1b1
Lyndell <lyndell.dobluis@gmail.com>
2023-12-04 19:14:15 +0800

    add unit test coverage for my jobs screen

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -5,7 +5,7 @@ export const healtHostApi = createApi({
   baseQuery: fetchBaseQuery({baseUrl: 'https://run.mocky.io/v3/'}),
   endpoints: builder => ({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    getPopularCities: builder.query<any>({
+    getPopularCities: builder.query<any, any>({
       query: () => '5e58bb33-dcf4-4d68-8a64-c68775955769',
     }),
     getSuggestedJobs: builder.query({
