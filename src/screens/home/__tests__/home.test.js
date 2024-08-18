cd2973dd6e80e127e3d3a95e623cf4ea86589ce2
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-08-18 17:21:34 +0800

    add unit test coverage for on boarding screens

--- src/screens/home/__tests__/home.test.js
+++ src/screens/home/__tests__/home.test.js
@@ -4,23 +4,6 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 import {renderWithProviders} from '@/utils/testframeworknew';
 import Home from '../home';
 
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
-jest.mock('@supabase/supabase-js')
-jest.useFakeTimers();
-jest.mock('@react-navigation/native', () => {
-  return {
-    ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      navigate: () => {},
-      addListener:()=>jest.fn()
-    }),
-    useIsFocused: () => true,
-    useDispatch: () => ({dispatch: jest.fn()}),
-  };
-});
-// jest.mock('@supabase/supabase-js');
-
 describe('Home Screen', () => {
   it('Should work as expected to get snapshot', () => {
     const all = renderWithProviders(<Home />);
--- src/screens/welcome/__tests__/welcome.test.js
+++ src/screens/welcome/__tests__/welcome.test.js
@@ -1,59 +1,34 @@
 import React from 'react';
-import {fireEvent, render} from '@testing-library/react-native';
-import {MockProvider} from '@/utils/testframework';
+import {fireEvent} from '@testing-library/react-native';
+import {renderWithProviders} from '@/utils/testframeworknew';
 import Welcome from '../welcome';
 
-jest.mock('@react-navigation/native', () => {
+jest.mock('../hooks', () => {
   return {
-    ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      navigate: () => {},
-    }),
-    useIsFocused: () => true,
-    useDispatch: () => ({dispatch: jest.fn()}),
+    UseWelcomeHooks: () => {
+      return {
+        handleSignIn: jest.fn(),
+        handleSignUp: jest.fn(),
+      };
+    },
   };
 });
 describe('Welcome Screen', () => {
   it('Should work as expected to get snapshot', () => {
-    const all = render(
-      <MockProvider
-        store={{
-          pcities: {loading: false, data: []},
-          suggetedjob: {loading: false, data: []},
-        }}>
-        <Welcome />
-      </MockProvider>,
-    );
+    const all = renderWithProviders(<Welcome />);
     expect(all.toJSON()).toMatchSnapshot();
   });
   it('Should work to trigger sign in button press', () => {
-    const all = render(
-      <MockProvider
-        store={{
-          pcities: {loading: false, data: []},
-          suggetedjob: {loading: false, data: []},
-        }}>
-        <Welcome />
-      </MockProvider>,
-    );
-    const el=all.getByTestId('WelcomeButtonGroupTestID')
-    fireEvent(el,'signInPress')
+    const all = renderWithProviders(<Welcome />);
+    const el = all.getByTestId('WelcomeButtonGroupTestID');
+    fireEvent(el, 'signInPress');
     expect(all.toJSON()).toBeTruthy();
   });
 
   it('Should work to trigger sign up button press', () => {
-    const all = render(
-      <MockProvider
-        store={{
-          pcities: {loading: false, data: []},
-          suggetedjob: {loading: false, data: []},
-        }}>
-        <Welcome />
-      </MockProvider>,
-    );
-    const el=all.getByTestId('WelcomeButtonGroupTestID')
-    fireEvent(el,'signUpPress')
+    const all = renderWithProviders(<Welcome />);
+    const el = all.getByTestId('WelcomeButtonGroupTestID');
+    fireEvent(el, 'signUpPress');
     expect(all.toJSON()).toBeTruthy();
   });
 });
