548d8c6ade8a198d860ccacd5ca3632e4bbe5de6
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-08-19 17:13:38 +0800

    add unit test coverage for on boarding bankinfo

---
+++ src/screens/onboarding/__tests__/bankInfo.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {renderWithProviders} from '@/utils/testframeworknew';
+import {waitFor} from '@testing-library/react-native';
+import {OnBoardingBankInfo} from '../index';
+
+describe('On boarding bankInfo Screen', () => {
+  it('Should work as expected to get snapshot', () => {
+    const all = renderWithProviders(<OnBoardingBankInfo />);
+    waitFor(() => {
+      expect(all.toJSON()).toMatchSnapshot();
+    });
+  });
+});
