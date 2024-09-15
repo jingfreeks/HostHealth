9aee4ac9166b160d81aea773edeb670d67d5f68a
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-09-15 11:57:46 +0800

    add user roles

--- src/slice/auth.tsx
+++ src/slice/auth.tsx
@@ -2,18 +2,20 @@ import {createSlice} from '@reduxjs/toolkit';
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: {user: null, token: null,userId:null},
+  initialState: {user: null, token: null,userId:null,roles:[]},
   reducers: {
     setCredentials: (state, action) => {
-      const {user, accessToken,userId} = action.payload;
+      const {user, accessToken,userId,roles} = action.payload;
       state.user = user;
       state.token = accessToken;
       state.userId = userId
+      state.roles=roles
     },
     setLogout: (state) => {
       state.user = null;
       state.token = null;
       state.userId = null;
+      state.roles=[]
     },
   },
 });
@@ -23,8 +25,7 @@ export const {setCredentials,setLogout}=authSlice.actions
 
 export default authSlice.reducer
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const selectCurrentUser = (state:any)=>state.auth.user
 export const selectCurrentUserId=(state:any)=>state.auth.userId
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const selectCurrentToken = (state:any)=>state.auth.token
+export const selectUserRoles=(state:any)=>state.auth.roles
