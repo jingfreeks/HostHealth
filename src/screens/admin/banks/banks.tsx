8f52c16590c4939140483cc8d769783eb30c54da
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-09-15 12:27:36 +0800

    admin setups

---
+++ src/screens/admin/banks/banks.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Banks = () => {
+  return (
+    <View>
+      <Text>Banks</Text>
+    </View>
+  )
+}
+
+export default Banks
